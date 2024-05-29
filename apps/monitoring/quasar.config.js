143874c924e0c558e472dfdbd14f331d377683d4
yyh <24493052+yongheng2016@users.noreply.github.com>
2024-05-29 13:47:03 +0800

    chore: fonts

--- apps/monitoring/quasar.config.js
+++ apps/monitoring/quasar.config.js
@@ -34,21 +34,10 @@ module.exports = configure(function (ctx) {
 		boot: ['i18n', 'axios', 'element-plus', 'bytetrade-ui'],
 
 		// https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-css
-		css: ['app.scss'],
+		css: ['app.scss', 'font.pro.scss'],
 
 		// https://github.com/quasarframework/quasar/tree/dev/extras
 		extras: [
-			// 'ionicons-v4',
-			// 'mdi-v5',
-			// 'fontawesome-v6',
-			// 'eva-icons',
-			// 'themify',
-			// 'line-awesome',
-			// 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
-			'roboto-font', // optional, you are not bound to it
-			'material-icons', // optional, you are not bound to it
-			'material-icons-outlined',
-			'bootstrap-icons',
 			'material-symbols-rounded'
 		],
 
