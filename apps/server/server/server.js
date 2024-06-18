580aa574061f672600629742da131d2407024a7a
yyh <24493052+yongheng2016@users.noreply.github.com>
2024-06-18 16:00:41 +0800

    fix: server port

--- apps/server/server/server.js
+++ apps/server/server/server.js
@@ -41,7 +41,7 @@ const app = new Koa();
 const serverConfig = getServerConfig().server;
 
 global.HOSTNAME = serverConfig.http.hostname || 'localhost';
-global.PORT = 8003
+global.PORT = serverConfig.http.port || 8000;
 
 app.keys = ['kubesphere->_<'];
 
